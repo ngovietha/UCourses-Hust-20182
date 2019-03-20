174a788bd0c73887626cb92d23ccf5b0aec80c95
phuongptm155 <minhphuonghc113@gmail.com>
2019-03-20 19:55:41 +0700

    create-model

--- UCourse/apps/models.py
+++ UCourse/apps/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from random import randint
 # Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length =255)
+    date = models.DateField()
+    des = models.CharField(max_length =255)
+    num = models.IntegerField(default = randint(0,50) )
+    view = models.IntegerField(default= randint(0,100) )
+    def __str__(self):
+        return self.name
