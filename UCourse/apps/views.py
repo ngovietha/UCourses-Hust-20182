f8ebf0522f6d22c4d2df2590cecec570d5ded186
quyctd <quy.dc98@gmail.com>
2019-03-26 20:22:34 +0700

    change homepage.html to index.html

--- UCourse/apps/views.py
+++ UCourse/apps/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def homepage(request):
     if request.method == "GET":
-        return render(request, "homepage.html")
+        return render(request, "index.html")
